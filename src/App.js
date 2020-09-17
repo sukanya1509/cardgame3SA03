fa924af6d91609ad4dc79eca21112d3844c0eecc
sukanya1509 <namsukanya1509@gmail.com>
2020-09-17 13:39:08 +0700

    change app js and create CharacterCard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
+const word = "Hello";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <CharacterCard value="h" />
-        <CharacterCard value="i" />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-
-    );
-  }
+import CharacterCard from './CharacterCard';
+function App() {
+ return (
+ <div>
+ <CharacterCard value="h"/>
+ <CharacterCard value="i"/>
+ </div>
+ );
 }
-
-export default App;
-
