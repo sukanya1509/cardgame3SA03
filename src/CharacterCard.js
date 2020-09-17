f5460c74d47bb8d46f02da8efe0030d62c9d5fd1
sukanya1509 <namsukanya1509@gmail.com>
2020-09-17 19:15:11 +0700

    coommit3

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -11,13 +11,18 @@ export default class CharacterCard extends Component {
         if (!this.state.active) {
             this.setState({ active: true})
         }else this.setState({ active: false})
+        }
+
     }
     activate = () => {
         if(!this.state.active){
         this.props.activationHandler(this.props.value)
         this.setState({active: true})
+            this.props.activationHandler(this.props.value)
+            this.setState({active: true})
         }
        }
+    }
 
     render() {
         let className = `card ${this.state.active ? 'activeCard' : ''}`
