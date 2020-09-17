839bb2206f27eea47e80d2af3739b8574ac13983
sukanya1509 <namsukanya1509@gmail.com>
2020-09-17 13:40:54 +0700

    change app.js from h i to Array

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import CharacterCard from './CharacterCard';
 const word = "Hello";
 
-import CharacterCard from './CharacterCard';
-function App() {
- return (
- <div>
- <CharacterCard value="h"/>
- <CharacterCard value="i"/>
- </div>
- );
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <CharacterCard value="h"/>
+          <CharacterCard value="i"/>
+      </div>
+        {
+          Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+        }
+     </div>
+    );
+  }
 }
+export default App;
