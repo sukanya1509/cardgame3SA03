81f058aa54404af56f59f387bd8b45b21acce3b1
sukanya1509 <namsukanya1509@gmail.com>
2020-09-17 14:07:31 +0700

    create file WordCard

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
+import CharacterCard from './WordCard';
 const word = "Hello";
 
 class App extends Component {
   render() {
     return (
-
-      <div className="card">
-  { Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)}
-     </div>
+      this.newMethod()
     );
   }
+
+  newMethod() {
+    return <div className="card">
+      {Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)}
+    </div>
+      ,
+      <div>
+        <WordCard value="hello" />
+      </div>;
+  }
 }
-
-
-export default App ;
+export default App;
---
+++ src/WordCard.js
@@ -0,0 +1,12 @@
+import CharacterCard from './CharacterCard';
+import React, { Component } from 'react';
+export default class WordCard extends
+Component {
+render() {
+    return (
+        <div>
+        <WordCard value="hello"/>
+        </div>
+        );
+}
+}
