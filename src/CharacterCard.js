e0618de83d954dcf672ffdfcda0ebfb57d46a907
sukanya1509 <namsukanya1509@gmail.com>
2020-09-17 19:11:07 +0700

    add  word app.css

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,25 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-export default class CharacterCard extends Component {
+export default class CharacterCard.js extends Component {
     constructor(props){
     super(props)
     this.state = {
     active: false,
+export default class CharacterCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            active: false,
+        }
     }
     }
+
 
     activate = () => {
     if(!this.state.active){
     this.setState({active: true, seq})
     }
+        if (!this.state.active) {
+            this.setState({ active: true})
+        }else this.setState({ active: false})
     }
     render(){
     let className = `card ${this.state.active ? 'activeCard': ''}`
     return (
-    <div>{this.props.value}
-    className={className} onClick={this.activate}{props.value}
+    <div className={className} onClick={this.activate}>
+    {this.props.value}
     </div>
     )
+    render() {
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
+        return (
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
+        )
     }
    } 
+}
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import CharacterCard from './CharacterCard';
+import CharacterCard from './CharacterCard.js';
+export default class WordCard.js extends
 export default class WordCard extends
-import CharacterCard from './CharacterCard';
-export default class WordCard extends;
-Component 
-render() 
+Component {
+render() {
  return (
- <div>
+ <div >
     {   Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
+
  </div>
  );
+ }
+}
  
