8c29d1a683abba17f8f4540ae06b7f0b7b04e940
sukanya1509 <namsukanya1509@gmail.com>
2020-09-17 13:56:53 +0700

    Array 1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
@@ -6,15 +7,13 @@ const word = "Hello";
 class App extends Component {
   render() {
     return (
-      <div>
-          <CharacterCard value="h"/>
-          <CharacterCard value="i"/>
-      </div>
-        {
-          Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
-        }
+
+      <div className="card">
+  { Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)}
      </div>
     );
   }
 }
-export default App;
+
+
+export default App ;
