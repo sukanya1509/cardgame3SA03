675a6737b167d5addf4e2f2390c38093a7df1d87
sukanya1509 <namsukanya1509@gmail.com>
2020-09-17 19:22:49 +0700

    add3

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,37 +1,60 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard.js';
-export default class WordCard extends
-Component {
-render() {
- return (
- <div >
-    {   Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
 
- </div>
- );
- }
-
+import _ from 'lodash';
+const prepareStateFromWord = (given_word) => {
+ let word = given_word.toUpperCase()
+@@ -16,9 +17,7 @@ const prepareStateFromWord = (given_word) => {
 export default class WordCard extends Component {
     constructor(props){
         super(props);
         this.state = {
             attempt: '',
         }
+        this.state = {}
+    }
+
+    componentWillMount(){
+@@ -32,15 +31,34 @@ export default class WordCard extends Component {
+        })
     }
 
 
     activationHandler = c => { 
         console.log(`${c} has been activated.`) 
+    activationHandler = (c) => {
+        let guess = [...this.state.guess, c]
+        this.setState({guess})
+        if(guess.length == this.state.chars.length){
+            if(this.state.word.match(guess.join('').toString())){
+                this.setState({
+                    guess: [], 
+                    completed: true})
+            }else{
+                this.setState({
+                    guess: [], 
+                    attempt: this.state.attempt + 1
+                })
+            }
+        }
     }
 
+
+    /*activationHandler = c => { 
+        console.log(`${c} has been activated.`);
+        console.log(this.state);
+    }*/
+
     render() {
+        console.log(this.state);
         return (
             <div >
                 { Array.from(this.props.value).map((c, i) => 
+                { this.state.chars.map((c, i) => 
                     <CharacterCard value={c} key={i} attempt={this.state.attempt}
                     activationHandler={this.activationHandler}/>                    
                 )}
             </div>
         );
     }
-} 
+}
