ef7fc7da7ba2975925a8fe22a2d0ab2cc917f818
sukanya1509 <namsukanya1509@gmail.com>
2020-09-17 12:57:06 +0700

    add hi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-function App() {
- return (
- <div>
- Hello World
- Hello World
- </div>
- );
+import CharacterCard from './CharacterCard';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CharacterCard value="h" />
+        <CharacterCard value="i" />
+      </div>
+
+    );
+  }
 }
+
 export default App;
+
