83215bd98411282bd06c40de7fc0f2c3cf41b116
sukanya1509 <namsukanya1509@gmail.com>
2020-09-17 19:12:06 +0700

    add word

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard.js';
-export default class WordCard.js extends
 export default class WordCard extends
 Component {
 render() {
@@ -11,5 +10,28 @@ render() {
  </div>
  );
  }
-}
- 
+
+export default class WordCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            attempt: '',
+        }
+    }
+
+
+    activationHandler = c => { 
+        console.log(`${c} has been activated.`) 
+    }
+
+    render() {
+        return (
+            <div >
+                { Array.from(this.props.value).map((c, i) => 
+                    <CharacterCard value={c} key={i} attempt={this.state.attempt}
+                    activationHandler={this.activationHandler}/>                    
+                )}
+            </div>
+        );
+    }
+} 
