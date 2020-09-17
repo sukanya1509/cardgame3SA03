30a0c66d00977d57517acaf85bfbba5edea58011
sukanya1509 <namsukanya1509@gmail.com>
2020-09-17 14:17:39 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import WordCard from './WordCard.js';
 class App extends Component {
   render() {
     return (
-      <div>
+
       <div className="card">
         <WordCard value="hello"/>
     </div>
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import CharacterCard from './CharacterCard';
+import CharacterCard from './CharacterCard.js';
 export default class WordCard extends
-Component {
-render() {
+import CharacterCard from './CharacterCard.js';
+export default class WordCard.js extends;
+Component 
+render() 
  return (
  <div>
-{ Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
     {   Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
-
  </div>
  );
- }
-}
+ 
